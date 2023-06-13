1a4650501db9ae28e4a31103ffdb63992a6d14d4
Nikita <sergeev_n_v@mail.ru>
2023-06-13 22:56:46 +0300

    Первый коммит. Добавлено главное окно.

---
+++ mainwindow.cpp
@@ -0,0 +1,7 @@
+#include "mainwindow.h"
+
+MainWindow::MainWindow()
+	: QMainWindow()
+{
+	setMinimumSize(700,500);
+}
---
+++ mainwindow.h
@@ -0,0 +1,13 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+class MainWindow : public QMainWindow
+{
+	Q_OBJECT
+public:
+	MainWindow();
+};
+
+#endif // MAINWINDOW_H
