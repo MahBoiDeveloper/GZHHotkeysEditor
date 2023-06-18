73eca4ca98d46774e3b43cc8e84bb305a406a8d5
MahBoiDeveloper <mah.boi.developer@gmail.com>
2023-06-19 00:02:51 +0300

    Initial methods for CSF class parser

--- src/CSFparser.cpp
+++ src/CSFparser.cpp
@@ -1,1 +1,38 @@
 #include "CSFparser.hpp"
+
+// Constructors and destructor
+CSFparser::CSFparser()
+{
+
+}
+
+CSFparser::CSFparser(string filePath)
+{
+
+}
+
+CSFparser::~CSFparser()
+{
+
+}
+
+// Methods
+bool InvertAllBytesInArray(uint8_t* array)
+{
+    return false;
+}
+
+bool IsASCII(string strSample)
+{
+    return false;
+}
+
+void Save()
+{
+
+}
+
+void Save(string strFileName)
+{
+
+}
--- src/CSFparser.hpp
+++ src/CSFparser.hpp
@@ -1,1 +1,32 @@
 #pragma once
+#include <string>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class CSFparser
+{
+private: // Data
+    const char* FSC {" FSC"};           // Begining of CSF file header
+    const char* LBL {" LBL"};           // Begining of any string name aka "label"
+    const char* LBL {" RTS"};           // Begining of any string value aka "string"
+    const char* LBL {"WRTS"};           // Begining of extra string value
+    const uint32_t CNC_CSF_VERSION = 3; // Standart file format. Legacy by WW
+
+    vector<string> Table;
+    vector<string> ExtraTable;
+private: // Private methods
+    void Parse();
+    void ParseHeader();
+    void ParseBody();
+
+    bool InvertAllBytesInArray(uint8_t* array);
+    bool IsASCII(string strSample);
+public: // Public methods
+    CSFparser();
+    CSFparser(string strFilePath);
+    ~CSFparser();
+    
+    void Save();
+    void Save(string strFileName);
+};
