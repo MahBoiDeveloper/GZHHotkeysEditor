02bcab09b8dbf9c46e2d0403221a143e02f8ccc2
MahBoiDeveloper <mah.boi.developer@gmail.com>
2023-06-20 23:28:26 +0300

    Try parse .csf file header

--- src/CSFparser.cpp
+++ src/CSFparser.cpp
@@ -1,38 +1,56 @@
 #include "CSFparser.hpp"
 
-// Constructors and destructor
-CSFparser::CSFparser()
+#pragma region Initialization and finilization
+    CSFparser::CSFparser()
 {
-
+    pTable = new list<CompiledString>();
+    pExtraTable = new list<ExtraCompiledString>();
 }
 
-CSFparser::CSFparser(string filePath)
-{
+    CSFparser::CSFparser(const string& filePath) : Path(filePath)
+    {
+        pTable = new list<CompiledString>();
+        pExtraTable = new list<ExtraCompiledString>();
+        Parse();
+    }
 
-}
+    CSFparser::~CSFparser()
+    {
+        delete pTable;
+        delete pExtraTable;
+    }
+#pragma endregion
 
-CSFparser::~CSFparser()
-{
+#pragma region Parsing
+    void CSFparser::Parse()
+    {
+        ifstream fileToParse{Path, ios::binary | ios::in};
 
-}
+        if(fileToParse.is_open())
+        {
+            fileToParse.read(reinterpret_cast<char*>(&Header), sizeof(CSFHeader));
 
-// Methods
-bool InvertAllBytesInArray(uint8_t* array)
+            for(;;)
+            {
+                
+            }
+        }
+
+        fileToParse.close();
+    }
+
+    void CSFparser::Save()
+    {
+
+    }
+
+    void CSFparser::Save(string strFileName)
+    {
+
+    }
+#pragma endregion
+
+bool CSFparser::IsASCII(string strSample)
 {
     return false;
-}
-
-bool IsASCII(string strSample)
-{
-    return false;
-}
-
-void Save()
-{
-
-}
-
-void Save(string strFileName)
-{
-
 }
--- src/CSFparser.hpp
+++ src/CSFparser.hpp
@@ -1,32 +1,78 @@
 #pragma once
 #include <string>
 #include <iostream>
-#include <vector>
+#include <fstream>
+#include <list>
 using namespace std;
+
+struct CompiledString
+{
+public:
+    string Name;
+    string Value;
+};
+
+struct ExtraCompiledString : CompiledString
+{
+public:
+    string ExtraValue;
+};
+
+struct CSFHeader
+{
+public:
+    uint8_t  csfChars[4];
+    uint32_t formatVersion;
+    uint32_t numberOfLabels;
+    uint32_t numberOfStrings;
+    uint32_t uselessBytes;
+    uint32_t languageCode;
+};
 
 class CSFparser
 {
 private: // Data
-    const char* FSC {" FSC"};           // Begining of CSF file header
-    const char* LBL {" LBL"};           // Begining of any string name aka "label"
-    const char* LBL {" RTS"};           // Begining of any string value aka "string"
-    const char* LBL {"WRTS"};           // Begining of extra string value
+    const char* FSC  = " FSC";          // Begining of CSF file header
+    const char* LBL  = " LBL";          // Begining of any string name aka "label"
+    const char* RTS  = " RTS";          // Begining of any string value aka "string"
+    const char* WRTS = "WRTS";          // Begining of extra string value
     const uint32_t CNC_CSF_VERSION = 3; // Standart file format. Legacy by WW
 
-    vector<string> Table;
-    vector<string> ExtraTable;
+    string Path;
+    CSFHeader Header;
+
+    list<CompiledString>* pTable;
+    list<ExtraCompiledString>* pExtraTable;
 private: // Private methods
     void Parse();
     void ParseHeader();
     void ParseBody();
 
-    bool InvertAllBytesInArray(uint8_t* array);
     bool IsASCII(string strSample);
 public: // Public methods
     CSFparser();
-    CSFparser(string strFilePath);
+    CSFparser(const string& strFilePath);
     ~CSFparser();
     
     void Save();
     void Save(string strFileName);
+
+    void AddString(CompiledString csString);
+    void AddString(list<CompiledString> csList);
+    void AddString(ExtraCompiledString csString);
+    void AddString(list<ExtraCompiledString> csList);
+    void AddEmptyString(string strName);
+
+    list<string> GetStringNames();
+    string GetStringValue(string strName);
+    list<string> GetExtraStringNames();
+    string GetExtraStringValue(string strName);
+
+    void ChangeStringName(string strOldName, string strNewName);
+    void ChangeStringValue(string strOldValue, string strNewValue);
+    
+    void DeleteString(string strName);
+    void DeleteString(CompiledString csString);
+
+    void ConvertExtraStringsToNormal();
 };
