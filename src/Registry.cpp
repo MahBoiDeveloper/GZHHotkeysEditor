16cfa02b285cab5dd150ef81ca2b1ee0248cb03b
MahBoiDeveloper <mah.boi.developer@gmail.com>
2023-07-27 20:09:02 +0300

    Add RootFolder enum and make refactoring

--- src/Registry.cpp
+++ src/Registry.cpp
@@ -22,32 +22,25 @@ Registry::WindowsBit Registry::GetWindowsBit()
 
     return windowsBit;
 }
-    
-/// @brief Returns REG_SZ string value from HKEY_LOCAL_MACHINE.
-string Registry::GetTextFromKeyInHKLM(const char* pPathToFolder, const char* pKeyName)
-{
-    HKEY rKey;
-    DWORD Size = 256;
-    TCHAR Reget[Size] = { 0 };
-        
-    RegOpenKeyExA(HKEY_LOCAL_MACHINE, pPathToFolder, 0, KEY_READ, &rKey);
-    RegQueryValueExA(rKey, pKeyName, NULL, NULL, (LPBYTE)&Reget, &Size);
-    RegCloseKey(rKey);
-    
-    string returnValue(Reget);
-    returnValue.shrink_to_fit();
-         
-    return returnValue;
-}
 
-/// @brief Returns REG_SZ string value from HKEY_CURRENT_USER.
-string Registry::GetTextFromKeyInHKCU(const char* pPathToFolder, const char* pKeyName)
+/// @brief Returns REG_SZ string value from MS Windows registry.
+string Registry::GetTextFromKey(const Registry::RootFolder Folder, const char* pPathToFolder, const char* pKeyName)
 {
     HKEY  rKey;
     DWORD Size = 256;
     TCHAR Reget[Size] = { 0 };
+
+    switch (Folder)
+    {
+        case Registry::RootFolder::HKCU :
+            RegOpenKeyExA(HKEY_CURRENT_USER, pPathToFolder, 0, KEY_READ, &rKey);
+            break;
         
-    RegOpenKeyExA(HKEY_CURRENT_USER, pPathToFolder, 0, KEY_READ, &rKey);
+        case Registry::RootFolder::HKLM :
+            RegOpenKeyExA(HKEY_LOCAL_MACHINE, pPathToFolder, 0, KEY_READ, &rKey);
+            break;
+    }
+
     RegQueryValueExA(rKey, pKeyName, NULL, NULL, (LPBYTE)&Reget, &Size);
     RegCloseKey(rKey);
     
@@ -58,13 +51,14 @@ string Registry::GetTextFromKeyInHKCU(const char* pPathToFolder, const char* pKe
 }
 
 /// @brief Sets paths to all games (C&C: Generals and C&C: Generals - Zero Hour).
-string Registry::GetPathToGame(Games game)
+string Registry::GetPathToGame(const Games game)
 {
     string Key  = "InstallPath";
     string Path = Registry::PATHS_TO_GAMES.find(game)->second.find(GetWindowsBit())->second;
-    return GetTextFromKeyInHKLM(Path.c_str(), Key.c_str());
+    return GetTextFromKey(Registry::RootFolder::HKLM, Path.c_str(), Key.c_str());
 }
 
+/// @brief Returns equal string for enum class value.
 string Registry::ToString(Games game)
 {
     string returnValue;
@@ -100,7 +94,7 @@ bool Registry::IsWindow32bit()
     {
         const char Path[] = {"Control Panel\\International\\Geo"};
         const char Key[]  = {"Name"};
-        return GetTextFromKeyInHKCU(&Path[0], &Key[0]);
+        return GetTextFromKey(Registry::RootFolder::HKCU, &Path[0], &Key[0]);
     }
 
     /// @brief Returns Windows version from HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\ProductName.
@@ -108,7 +102,7 @@ bool Registry::IsWindow32bit()
     {
         const char Path[] = {"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion"};
         const char Key[]  = {"ProductName"};
-        return GetTextFromKeyInHKLM(&Path[0], &Key[0]);
+        return GetTextFromKey(Registry::RootFolder::HKLM, &Path[0], &Key[0]);
     }
 
     /// @brief Returns processor vendor infomation from HKLM\\HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0\\ProcessorNameString.
@@ -116,6 +110,6 @@ bool Registry::IsWindow32bit()
     {
         const char Path[]  = {"HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0"};
         const char Value[] = {"ProcessorNameString"};
-        return GetTextFromKeyInHKLM(&Path[0], &Value[0]);
+        return GetTextFromKey(Registry::RootFolder::HKLM, &Path[0], &Value[0]);
     }
 #pragma endregion
--- src/Registry.hpp
+++ src/Registry.hpp
@@ -4,7 +4,7 @@
 
 class Registry final
 {
-public: // Data
+public: // Data types declarations
     enum class WindowsBit
     {
         Win32 = 0,
@@ -17,6 +17,12 @@ public: // Data
         GeneralsZeroHour
     };
 
+    enum class RootFolder
+    {
+        HKLM = 0,
+        HKCU
+    };
+public: // Data
     inline static const std::map<Games, std::map<WindowsBit, std::string>> PATHS_TO_GAMES =
     {
         {Games::Generals,         {{WindowsBit::Win32, "SOFTWARE\\Electronic Arts\\EA Games\\Generals"},
@@ -31,13 +37,12 @@ public: // Methods
     static std::string ToString(Games game);
     static WindowsBit  GetWindowsBit();
     
-    static std::string GetTextFromKeyInHKLM(const char* pPathToFolder, const char* pKeyName);
-    static std::string GetTextFromKeyInHKCU(const char* pPathToFolder, const char* pKeyName);
+    static std::string GetTextFromKey(const RootFolder Folder, const char* pPathToFolder, const char* pKeyName);
 
     static bool IsWindow64bit();
     static bool IsWindow32bit();
 
-    static std::string GetPathToGame(Games game);
+    static std::string GetPathToGame(const Games game);
     static std::string GetCurrentUserLanguage();
     static std::string GetWindowsVersion();
     static std::string GetProcessorInfo();
