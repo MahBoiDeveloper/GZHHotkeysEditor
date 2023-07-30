d2a7a9b9d3fadb75428e4b4a5191580698a52611
Nikita <sergeev_n_v@mail.ru>
2023-07-30 11:44:53 +0300

    Finally fixed translation files generation logic.

--- src/gui/translations/ru.ts
+++ src/gui/translations/ru.ts
@@ -2,80 +2,67 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ru_RU">
 <context>
-    <name>CreatorWidget</name>
+    <name>BaseConfigurationWidget</name>
     <message>
-        <source>Save hotkey dirrectly to the game</source>
-        <translation type="vanished">Сохранять горячие клавиши в игре</translation>
-    </message>
-    <message>
-        <source>Start configurating</source>
-        <translation type="vanished">Начать настройку</translation>
-    </message>
-    <message>
-        <source>Save hotkeys dirrectly to the game</source>
-        <translation type="vanished">Сохранять горячие клавиши в игре</translation>
-    </message>
-    <message>
-        <location filename="../initializationWidgets/creatorwidget.cpp" line="27"/>
-        <source>Save hotkeys dirrectly to the game.</source>
-        <translation>Сохранять горячие клавиши в игре</translation>
-    </message>
-    <message>
-        <location filename="../initializationWidgets/creatorwidget.cpp" line="31"/>
+        <location filename="../initializationWidgets/baseconfigurationwidget.cpp" line="9"/>
         <source>Configure</source>
         <translation>Настройка</translation>
     </message>
     <message>
-        <location filename="../initializationWidgets/creatorwidget.cpp" line="32"/>
+        <location filename="../initializationWidgets/baseconfigurationwidget.cpp" line="10"/>
         <source>Back</source>
         <translation>Назад</translation>
     </message>
 </context>
 <context>
+    <name>CreatorWidget</name>
+    <message>
+        <location filename="../initializationWidgets/creatorwidget.cpp" line="28"/>
+        <source>Save hotkeys dirrectly to the game.</source>
+        <translation>Сохранять горячие клавиши в игре.</translation>
+    </message>
+</context>
+<context>
     <name>Editor</name>
     <message>
-        <location filename="../editor/editor.cpp" line="19"/>
+        <location filename="../editor/editor.cpp" line="20"/>
         <source>File</source>
         <translation>Файл</translation>
     </message>
     <message>
-        <location filename="../editor/editor.cpp" line="20"/>
+        <location filename="../editor/editor.cpp" line="21"/>
         <source>Special</source>
         <translation>Доп</translation>
     </message>
     <message>
-        <location filename="../editor/editor.cpp" line="22"/>
+        <location filename="../editor/editor.cpp" line="23"/>
         <source>View</source>
         <translation>Вид</translation>
     </message>
     <message>
-        <location filename="../editor/editor.cpp" line="23"/>
+        <location filename="../editor/editor.cpp" line="24"/>
         <source>Settings</source>
         <translation>Настройки</translation>
     </message>
     <message>
-        <location filename="../editor/editor.cpp" line="25"/>
-        <location filename="../editor/editor.cpp" line="91"/>
+        <location filename="../editor/editor.cpp" line="26"/>
+        <location filename="../editor/editor.cpp" line="90"/>
         <source>About</source>
         <translation>О программе</translation>
     </message>
     <message>
-        <location filename="../editor/editor.cpp" line="76"/>
+        <location filename="../editor/editor.cpp" line="73"/>
         <source>Authors: </source>
         <translation>Авторы: </translation>
     </message>
     <message>
-        <location filename="../editor/editor.cpp" line="83"/>
+        <location filename="../editor/editor.cpp" line="80"/>
         <source>Program licensed by GNU GPL v3</source>
         <translation>Программа лицензированапод лицензией GNU GPL v3</translation>
     </message>
 </context>
 <context>
     <name>HotkeyElement</name>
-    <message>
-        <source>press the letter key</source>
-        <translation type="vanished">Нажмите любую клавишу...</translation>
-    </message>
     <message>
         <location filename="../editor/hotkeyelement.cpp" line="59"/>
         <source>Press any key...</source>
@@ -105,58 +92,32 @@
         <translation>Загрузить проект</translation>
     </message>
     <message>
-        <location filename="../initializationWidgets/loaderwidget.cpp" line="56"/>
-        <source>Configure</source>
-        <translation>Настройка</translation>
-    </message>
-    <message>
-        <source>Load hotkey map from file</source>
-        <translation type="vanished">Загрузить проект</translation>
-    </message>
-    <message>
         <location filename="../initializationWidgets/loaderwidget.cpp" line="40"/>
         <source>Load installed game hotkey map</source>
         <translation>Загрузить горячие клавиши из игры</translation>
-    </message>
-    <message>
-        <source>Start configurating</source>
-        <translation type="vanished">Начать настройку</translation>
-    </message>
-    <message>
-        <location filename="../initializationWidgets/loaderwidget.cpp" line="57"/>
-        <source>Back</source>
-        <translation>Назад</translation>
     </message>
 </context>
 <context>
     <name>StartWidget</name>
     <message>
-        <source>NEW HOTKEY MAP</source>
-        <translation type="vanished">Новый проект</translation>
-    </message>
-    <message>
-        <source>LOAD HOTKEY MAP</source>
-        <translation type="vanished">Загрузить проект</translation>
-    </message>
-    <message>
-        <location filename="../initializationWidgets/startwidget.cpp" line="26"/>
+        <location filename="../initializationWidgets/startwidget.cpp" line="24"/>
         <source>New project</source>
         <translation>Новый проект</translation>
     </message>
     <message>
-        <location filename="../initializationWidgets/startwidget.cpp" line="29"/>
+        <location filename="../initializationWidgets/startwidget.cpp" line="27"/>
         <source>Load project</source>
         <translation>Загрузить проект</translation>
     </message>
     <message>
-        <location filename="../initializationWidgets/startwidget.cpp" line="56"/>
+        <location filename="../initializationWidgets/startwidget.cpp" line="51"/>
         <source>Language</source>
         <translation>Язык</translation>
     </message>
     <message>
-        <location filename="../initializationWidgets/startwidget.cpp" line="65"/>
-        <source>Greetings, my friend.</source>
-        <translation type="unfinished"></translation>
+        <location filename="../initializationWidgets/startwidget.cpp" line="61"/>
+        <source>Greetings, my friend. There is an amazing super cool program for editing hotkeys.</source>
+        <translation>Приветствую тебя, мой друг. Это потрясающая, супер крутая программа для редактирования горячих клавиш.</translation>
     </message>
 </context>
 </TS>
