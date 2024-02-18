517e5f784b64ab902b7ab13d431c2065ba7595d6
MahBoiDeveloper <mah.boi.developer@gmail.com>
2024-02-18 13:38:09 +0300

    Add batch-file for compilation without editor

---
+++ zBuild.bat
@@ -0,0 +1,21 @@
+@echo off
+chcp 65001 > nul
+
+echo Generals Zero Hour Hotkeys editor
+echo Authors: mah_boi, nikitvs
+echo.
+
+echo Configuring MinGW Make...
+cmake -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=C:\Qt\Tools\mingw810_64\bin\gcc.exe -DCMAKE_CXX_COMPILER:FILEPATH=C:\Qt\Tools\mingw810_64\bin\g++.exe -S.\ -B.\build -G "MinGW Makefiles"
+echo Configuring MinGW Make done
+echo.
+
+echo Compilation and linking executables...
+cmake --build .\build --config Release --target all -j 4
+echo Compilation and linking executables done
+echo.
+
+echo Starting editor in 5 seconds
+timeout /t 5
+cd .\build\exe\
+start "" HotkeyEditor.exe
