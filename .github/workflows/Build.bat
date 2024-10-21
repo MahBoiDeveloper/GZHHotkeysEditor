acbdd3d36e19e2cab65274c4547588d7bac9037c
mah_boi <61310813+MahBoiDeveloper@users.noreply.github.com>
2024-10-21 21:54:56 +0300

    Replace g++.exe with x86_64-w64-mingw32-g++.exe

--- .github/workflows/Build.bat
+++ .github/workflows/Build.bat
@@ -23,7 +23,7 @@ echo Authors: mah_boi, nikitvs
 echo.
 
 echo Configuring MinGW Make...
-cmake -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=C:\Qt\Tools\mingw_64\bin\gcc.exe -DCMAKE_CXX_COMPILER:FILEPATH=C:\Qt\Tools\mingw_64\bin\g++.exe -S.\ -B.\build -G "MinGW Makefiles"
+cmake -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=C:\Qt\Tools\mingw_64\bin\x86_64-w64-mingw32-gcc.exe -DCMAKE_CXX_COMPILER:FILEPATH=C:\Qt\Tools\mingw_64\bin\x86_64-w64-mingw32-g++.exe -S.\ -B.\build -G "MinGW Makefiles"
 echo Configuring MinGW Make done
 echo.
 
