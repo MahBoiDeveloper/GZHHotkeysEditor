0b3b390f26dbdbb07680b1ae4c9e64a99b959783
MahBoiDeveloper <mah.boi.developer@gmail.com>
2024-11-01 14:15:56 +0300

    Implement simple folder check system

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,6 +2,7 @@
 #include <fcntl.h>   // Allows to use UTF-16 encoding as the default encoding
 #include <windows.h> // Allows disable console
 #include <iostream>
+#include <filesystem>
 
 // Qt headers
 #include <QApplication>
@@ -17,19 +18,22 @@
 
 using namespace std;
 
-int main(int argc, char** argv)
+int main(int argc, const char** argv)
 {
+    // After this all out text to console MUST be showed via std::wcout and all chars should be converted as wchar_t
+    _setmode(_fileno(stdout), _O_U16TEXT);
+    
+    if (!filesystem::exists("..\\..\\Logs"))
+        filesystem::create_directory("..\\..\\Logs");
+
     JSONFile settings(SETTINGS_PATH);
 
     // Hides console
     if (!settings.Query("$.DebugConsole").toBool()) 
         ShowWindow(GetConsoleWindow(), SW_HIDE);
-    
-    // After this all out text to console MUST be showed via std::wcout and all chars should be converted as wchar_t
-    _setmode(_fileno(stdout), _O_U16TEXT);
 
     // Initialize main qt application
-    QApplication HotkeyEditor(argc, argv);
+    QApplication HotkeyEditor(argc, const_cast<char**>(argv));
 
     // Define logger as a singleton class, that could be used anywhere in project
     WINDOW_MANAGER = make_unique<WindowManager>();
--- src/MainStarter.cpp
+++ src/MainStarter.cpp
@@ -1,28 +1,54 @@
 #ifdef PROJECT_EXE_RELATIVE_PATH
-    
-    #include <cstdlib>
-    #include <string>
-    
-    // Links local executable to the main application of the project
-    int main(int argc, const char** argv)
+
+#define EXCEPTION_HEADER "I'VE GOT A PRESENT FOR YA"
+#define RESOURCES "Resources"
+#define BINARIES "Resources\\Binaries"
+
+#include <cstdlib>
+#include <string>
+#include <filesystem>
+#include <iostream>
+// #include <QMessageBox>
+
+// Links local executable to the main application of the project
+int main(int argc, const char** argv)
+{
+    // Create /Logs if they doesn't exists
+    std::filesystem::create_directory("Logs");
+
+    if (!std::filesystem::exists(RESOURCES))
     {
-        std::string projectExecCommand(PROJECT_EXE_RELATIVE_PATH);
-    
-        // Replacing the forward slash with the reverse in Windows
-        size_t pos = 0;
-    
-        while ((pos = projectExecCommand.find('/',pos)) != std::string::npos)
-            projectExecCommand.replace(pos++, 1, "\\\\");
-    
-        // Command formation
-        for (int i = 0; i < argc; i++)
-        {
-            projectExecCommand += " ";
-            projectExecCommand += argv[i];
-        }
-    
-        // Calling the main executable
-        return system(projectExecCommand.c_str());
+        std::cout << "Folder \""<< RESOURCES <<"\" doesn't exist!" << std::endl 
+                  << "You cannot use Hotkey Editor without configs and Qt binary files." << std::endl
+                  << "Check your installation instruction." << std::endl;
+        // QMessageBox::critical(nullptr, EXCEPTION_HEADER, "Folder \"Resources\" is empty!\n Checkout installation instruction.");
+        return -1;
     }
 
+    if (!std::filesystem::exists(BINARIES))
+    {
+        std::cout << "Folder \"" << BINARIES << "\" doesn't exist!" << std::endl 
+                  << "You cannot use Hotkey Editor without Qt binary files." << std::endl
+                  << "Check your installation instruction." << std::endl;
+        // QMessageBox::critical(nullptr, EXCEPTION_HEADER, "Folder \"Resources\" is empty!\n Checkout installation instruction.");
+        return -1;
+    }
+    
+    std::string realExecutablePath(PROJECT_EXE_RELATIVE_PATH);
+
+    // Replace the forward slash with the reverse slash
+    size_t pos = 0;
+
+    // Replace Cmake's path with '/' to '\\'
+    while ((pos = realExecutablePath.find('/',pos)) != std::string::npos)
+        realExecutablePath.replace(pos++, 1, "\\\\");
+
+    // Add commands from argv
+    for (int i = 0; i < argc; i++)
+        realExecutablePath += std::string(" ") + std::string(argv[i]);
+
+    // Call the main executable
+    return system(realExecutablePath.c_str());
+}
 #endif
+
